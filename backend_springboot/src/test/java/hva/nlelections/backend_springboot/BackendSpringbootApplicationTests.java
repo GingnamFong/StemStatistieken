26cc76ca35a0857653059da597fdd8fb308bd663
KevinKhd <kevinkhd03@gmail.com>
2025-09-25 09:59:16 +0200

    Project setup

---
+++ backend_springboot/src/test/java/hva/nlelections/backend_springboot/BackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package hva.nlelections.backend_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
