d7d82aba03b70d82df4811695060d2a0db77d276
KevinKhd <kevinkhd03@gmail.com>
2025-10-08 12:39:07 +0200

    Hero text + photo banner with buttons for election results per year.

--- frontend_vue/src/views/HomeView.vue
+++ frontend_vue/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import YearBanner from '../components/YearBanner.vue'
 </script>
 
 <template>
   <main>
+    <YearBanner background-image-url="/images/banner.png" />
     <TheWelcome />
   </main>
 </template>
