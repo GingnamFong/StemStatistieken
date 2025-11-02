860d7fa3e67d1ff38b6584351c42c4c586e3ea77
KevinKhd <kevinkhd03@gmail.com>
2025-11-02 23:59:01 +0100

    fix(frontend): voorkom dubbele component rendering

--- frontend_vue/src/App.vue
+++ frontend_vue/src/App.vue
@@ -2,14 +2,12 @@
 import { RouterView } from 'vue-router'
 import TopNavbar from './components/TopNavbar.vue'
 import OnboardingGuide from "@/components/OnboardingGuide.vue";
-import HomeView from "@/views/HomeView.vue";
 </script>
 
 <template>
   <TopNavbar />
 
   <main>
-    <HomeView />
     <RouterView />
   </main>
 
@@ -23,6 +21,6 @@ body {
 }
 
 main {
-  padding: 20px;
+  padding: 0;
 }
 </style>
