999651c8abfe09a6f80c13273cf76aa392d1c9a0
Jonathan Roemers <jonathan.roemers@hva.nl>
2025-11-03 23:49:44 +0100

    national

--- XMLParser/src/main/java/nl/hva/ict/sm3/backend/model/National.java
+++ XMLParser/src/main/java/nl/hva/ict/sm3/backend/model/National.java
@@ -3,20 +3,16 @@ package nl.hva.ict.sm3.backend.model;
 public class National {
     private String id;
     private String party;
-    private String region;
+    private String national;
 
-    public National (String id,String name, String region) {
+    public National (String id,String name, String national) {
         this.id = id;
         this.party = name;
-        this.region = region;
+        this.national = national;
     }
 
     public String getId() { return id; }
     public String getParty() { return party; }
-    public String getRegion() { return region; }
+    public String getNational() { return national; }
 
-    @Override
-    public String toString() {
-        return "Party{id='%s', name='%s', region=%s}".formatted(id, party, region);
-    }
 }
