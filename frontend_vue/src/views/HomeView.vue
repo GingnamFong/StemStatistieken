2567cd14a4717d4f6d74516138d558acf3239abb
GingFong <90309841+GingnamFong@users.noreply.github.com>
2025-11-25 16:06:32 +0100

    icoon change

--- frontend_vue/src/views/HomeView.vue
+++ frontend_vue/src/views/HomeView.vue
@@ -79,7 +79,7 @@ const navigateToCompare = () => {
         <div class="feature-card">
           <div class="feature-icon" style="background: #3b82f6;">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
+              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.94-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
             </svg>
           </div>
           <h3 class="feature-title">Interactieve Kaarten</h3>
