d660ec01055ba167f71436e1f156e17c151cb34f
Jerrico Garcia <jerrico.garcia@hva.nl>
2025-12-14 20:42:29 +0100

    added jpa repository for forum comment

---
+++ XMLParser/src/main/java/nl/hva/ict/sm3/backend/repository/ForumCommentRepository.java
@@ -0,0 +1,10 @@
+package nl.hva.ict.sm3.backend.repository;
+
+import nl.hva.ict.sm3.backend.model.ForumComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ForumCommentRepository extends JpaRepository<ForumComment, Long> {
+    List<ForumComment> findByPostIdOrderByCreatedAtAsc(Long postId);
+}
