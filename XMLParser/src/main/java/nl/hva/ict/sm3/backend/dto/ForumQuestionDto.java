3a4c3d20f310e4dade56ea1002bf00bc5217023b
Jonathan Roemers <jonathan.roemers@hva.nl>
2025-12-15 15:04:24 +0100

    ForumQuestionDto dto

--- XMLParser/src/main/java/nl/hva/ict/sm3/backend/dto/ForumQuestionDto.java
+++ XMLParser/src/main/java/nl/hva/ict/sm3/backend/dto/ForumQuestionDto.java
@@ -1,7 +1,20 @@
 package nl.hva.ict.sm3.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class ForumQuestionDto {
     public String getBody() {
+
         return "";
     }
+
+    @NotBlank
+    private String body;
+
+    public ForumQuestionDto() {
+
+    }
+
+    public void setBody(String body) {this.body = body;}
 }
